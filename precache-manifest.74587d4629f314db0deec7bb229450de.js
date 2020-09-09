15ffeac632a2f2f272d41a4b89ea26a8cf87ca1f
Ruslan Shayakhmetov <ruslan.shayakhmetov@edetek.com>
2020-09-10 02:07:44 +0600

    Updates

---
+++ precache-manifest.74587d4629f314db0deec7bb229450de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058f6db0457c0a7e9bef106b547485e9",
+    "url": "/react-ui-kit/index.html"
+  },
+  {
+    "revision": "e170d1dfdb987dfb2eef",
+    "url": "/react-ui-kit/static/css/main.ead367d9.chunk.css"
+  },
+  {
+    "revision": "8b08051c27c2fe953c59",
+    "url": "/react-ui-kit/static/js/2.6c376c67.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-ui-kit/static/js/2.6c376c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e170d1dfdb987dfb2eef",
+    "url": "/react-ui-kit/static/js/main.6dbec94f.chunk.js"
+  },
+  {
+    "revision": "4dcd348f19116dcff0e0",
+    "url": "/react-ui-kit/static/js/runtime-main.067317d1.js"
+  }
+]);
